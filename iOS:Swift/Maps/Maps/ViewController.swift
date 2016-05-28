ab0aff31ef96ecf4b14c3412e8aef40e1d9bfa5f
Ian Dorosh <iDorosh@Ians-MBP.T-mobile.com>
2016-05-27 19:15:32 -0500

    iOS

---
+++ iOS:Swift/Maps/Maps/ViewController.swift
@@ -0,0 +1,169 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Ian Dorosh on 4/12/16.
+//  Copyright © 2016 Vulkan Mobile Development. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
+class ViewController: UIViewController, CLLocationManagerDelegate, UISearchControllerDelegate {
+    
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var selectView: UISegmentedControl!
+    
+    let locationManager = CLLocationManager()
+    
+    var resultSearchController:UISearchController? = nil
+    var selectedPin:MKPlacemark? = nil
+    
+    var lat : Double = Double()
+    var lon : Double = Double()
+   
+    
+    @IBAction func currentLocation(sender: UIButton) {
+        zoom = false
+    }
+    
+    @IBAction func viewSelected(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            mapView.mapType = MKMapType.Standard
+        } else if sender.selectedSegmentIndex == 1{
+            mapView.mapType = MKMapType.Satellite
+        } else if sender.selectedSegmentIndex == 2{
+            mapView.mapType = MKMapType.Hybrid
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = mapView
+        
+        locationSearchTable.handleMapSearchDelegate = self
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var zoom : Bool = false
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        lat = locValue.latitude
+        lon = locValue.longitude
+        if (!zoom){
+            zoom = true
+            movePosition()
+        }
+    }
+    
+    func getDirections(){
+        if let selectedPin = selectedPin {
+            let mapItem = MKMapItem(placemark: selectedPin)
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+            mapItem.openInMapsWithLaunchOptions(launchOptions)
+        }
+    }
+    
+    func movePosition(){
+        let latitude:CLLocationDegrees = lat
+        
+        let longitude:CLLocationDegrees = lon
+        
+        let latDelta:CLLocationDegrees = 0.05
+        
+        let lonDelta:CLLocationDegrees = 0.05
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        mapView.showsUserLocation = true
+        
+        mapView.setRegion(region, animated: false)
+    }
+    
+}
+
+extension ViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city) \(state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension ViewController : MKMapViewDelegate {
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView?.pinTintColor = UIColor.orangeColor()
+        pinView?.canShowCallout = true
+        let smallSquare = CGSize(width: 30, height: 30)
+        let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+        button.setBackgroundImage(UIImage(named: "car"), forState: .Normal)
+        button.addTarget(self, action: #selector(ViewController.getDirections), forControlEvents: .TouchUpInside)
+        pinView?.leftCalloutAccessoryView = button
+        return pinView
+    }
+}
+
+
